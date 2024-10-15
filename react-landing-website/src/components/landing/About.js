f0a5a0dc492c3206b2045b34138db60055f2ea33
Aviwe <115289965+AC-Baleni@users.noreply.github.com>
2024-10-15 23:38:40 +0200

    Update About.js

--- react-landing-website/src/components/landing/About.js
+++ react-landing-website/src/components/landing/About.js
@@ -1,12 +1,22 @@
 import React from 'react'
+import './About.css'
 
 const About = () => {
   return (
-    <div>
-      <img src="/assets/about-pic.jpg" alt="About picture"></img>
-      <div>
-        
+    <div className='aboutContainer'>
+      <div className='gameCard'>
+        <h2>Game Development</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vel sollicitudin neque, et tempus velit. Donec vel nunc non ex sagittis convallis.</p>
       </div>
+      <div className='chatCard'>
+        <h2>Chat Application</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vel sollicitudin neque, et tempus velit. Donec vel nunc non ex sagittis convallis.</p>
+      </div>
+      <div className='postsCard'>
+        <h2>Social Media Posts</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vel sollicitudin neque, et tempus velit. Donec vel nunc non ex sagittis convallis.</p>
+      </div>
+
     </div>
   )
 }
