374f9943b8933ab4ab4970a0897a9cf6e98ea213
Aviwe <115289965+AC-Baleni@users.noreply.github.com>
2024-10-16 23:37:20 +0200

    Update About.js

--- react-landing-website/src/components/landing/About.js
+++ react-landing-website/src/components/landing/About.js
@@ -6,7 +6,7 @@ const About = () => {
     <div className='aboutContainer'>
       <div className='gameCard'>
         <h2>Game Development</h2>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vel sollicitudin neque, et tempus velit. Donec vel nunc non ex sagittis convallis.</p>
+        <p>This is for the vibes, consectetur adipiscing elit. Sed vel sollicitudin neque, et tempus velit. Donec vel nunc non ex sagittis convallis.</p>
       </div>
       <div className='chatCard'>
         <h2>Chat Application</h2>
